f8a06b1b541289cb199bb828c69b3d3b932b6ec6
iveiks <iveiks@gmail.com>
2025-10-09 14:39:58 +0300

    Add files via upload

---
+++ h02.py
@@ -0,0 +1,45 @@
+import turtle
+
+#kilpkonna seaded
+turtle.speed(0)
+ekraan = turtle.Screen()
+ekraan.title("Olümpiarõngad Veiko")
+ekraan.setup(500,400)
+turtle.pensize(6)
+
+# olümpiarõngad
+turtle.pencolor("blue")
+turtle.penup()
+turtle.goto(-110,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("black")
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("red")
+turtle.penup()
+turtle.goto(110,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("yellow")
+turtle.penup()
+turtle.goto(-55,-55)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("green")
+turtle.penup()
+turtle.goto(55,-45)
+turtle.pendown()
+turtle.circle(50)
+
+
+
+
+
+turtle.done()
