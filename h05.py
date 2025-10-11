bd38d2187f3086312ec77a7b794311e9d257b846
iveiks <iveiks@gmail.com>
2025-10-11 12:42:29 +0300

    harjutus 5.3

--- h05.py
+++ h05.py
@@ -1,3 +1,23 @@
+import random
+# Matemaatika test (randint)
+
+# Kirjuta programm, mis kontrollib kasutaja poolt sisestatud vastust lihtsale matemaatikaülesandele.
+# Näiteks, programm esitab küsimuse “Mis on 3 korda 4?”. Kasutaja peab sisestama vastuse.
+# Kui kasutaja vastus on 12, siis programm väljastab “Õige vastus!”, kui aga vastus on midagi muud, siis väljastab “Vale vastus, proovi uuesti!”.
+# Kasuta if ja else lauseid selleks, et kontrollida kasutaja sisestatud vastust.
+
+arv1 = random.randint(1,10)
+arv2 = random.randint(1,10)
+tehe = arv1 * arv2
+vastus = int(input(f"{arv1} x {arv2} = "))
+if vastus == tehe :
+    print("Õige vastus!")
+else :
+    print("Vale vastus!")
+
+
+
+
 # Vanusepiiranguga üritus
 
 # Sa oled loomas programmi, mis aitab kontrollida, kas inimesed vastavad vanusepiirangu nõuetele üritusel osalemiseks.
@@ -12,3 +32,6 @@ if vanus >= 18 :
     print("Võid siseneda")
 else :
     print("Ei ole piisavalt vana")
+    
+    
+    
