7806c8ebfd62d547eeb9cde41d59643a8750b20a
iveiks <iveiks@gmail.com>
2025-10-27 19:22:22 +0200

    Tehtud iseseisev töö 2

--- is2.py
+++ is2.py
@@ -1,1 +1,30 @@
 import turtle
+
+for x in range(4):
+  # Üks tsükkel
+  turtle.fd(100)
+  turtle.lt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.lt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.lt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.lt(90)
+  turtle.fd(100)
+  turtle.rt(90)
+  turtle.fd(100)
+  turtle.rt(180)
+
+turtle.done()
