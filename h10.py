ec4fcc1ea5583a1e4df47d0505499ea6d86fd9bc
iveiks <iveiks@gmail.com>
2025-11-06 13:39:25 +0200

    Tehtud harjutus 10

--- h10.py
+++ h10.py
@@ -6,3 +6,17 @@
 # kuni kasutaja jätab sisestuse tühjaks (st vajutab sisestusklahvi ilma midagi kirjutamata).
 # Programm peab kasutama while-tsüklit arvude küsimiseks ja nende salvestamiseks.
 # Pärast seda, kui kasutaja lõpetab arvude sisestamise peab programm arvutama ja väljastama kõikide sisestatud arvude keskmise väärtuse.
+
+loop = 1
+counter = 0
+sum = 0
+
+while loop:
+    arv = input("Lisa arv: ")
+    if arv == "":
+        break
+    counter += 1
+    sum += int(arv)
+
+print(f"Keskmine: {sum/counter}")
+
