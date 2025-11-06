363bd49442c44fcc114278f039ad6131ac5c2fa3
iveiks <iveiks@gmail.com>
2025-11-06 13:26:38 +0200

    Loodud harjutus 10

---
+++ h10.py
@@ -0,0 +1,8 @@
+# Harjutus 10
+
+# 1 Arvude keskmine
+# Koostage Pythoni programm, mis küsib kasutajalt arve ükshaaval.
+# Programm peaks jätkama arvude küsimist ja nende vastuvõtmist seni,
+# kuni kasutaja jätab sisestuse tühjaks (st vajutab sisestusklahvi ilma midagi kirjutamata).
+# Programm peab kasutama while-tsüklit arvude küsimiseks ja nende salvestamiseks.
+# Pärast seda, kui kasutaja lõpetab arvude sisestamise peab programm arvutama ja väljastama kõikide sisestatud arvude keskmise väärtuse.
